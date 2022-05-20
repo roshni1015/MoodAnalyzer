3082d1cd51f61345ef8925aa5087e82d220f6c04
roshni1015 <roshniadatrao1110@gmail.com>
2022-05-20 21:58:53 +0530

    [Roshni]-Test cases For Analysing Mood

---
+++ MoodAnalyzer/MoodAnalyzerTests/MoodAnalyserTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MoodAnalyzer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyzer.Tests
+{
+    [TestClass()]
+    public class MoodAnalyserTests
+    {
+        [TestMethod()]
+        public void analyseMoodTest()
+        {
+            MoodAnalyser analyse = new();
+            Assert.AreEqual("Happy", analyse.analyseMood("I am in Happy Mood"));
+        }
+    }
+}
