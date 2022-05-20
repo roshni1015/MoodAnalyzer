0c08fc92661493c2b969f88759cd9f315410f071
roshni1015 <roshniadatrao1110@gmail.com>
2022-05-20 22:30:43 +0530

    [Roshni]-Using Default And Parameterized Constructor

---
+++ MoodAnalyzer/MoodAnalyzer/MoodAnalyzerusingConstructor.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyzer
+{
+    public class MoodAnalyzerusingConstructor
+    {
+        public MoodAnalyzerusingConstructor()
+        {
+
+        }
+
+        public string msg1 = "I am in Sad Mood";
+        public MoodAnalyzerusingConstructor(string message)
+        {
+            this.msg1 = message;
+        }
+        public string analyseMood1()
+        {
+            string message1 = "Sad";
+            if (msg1.ToLower().Contains(message1.ToLower()))
+            {
+                return message1;
+            }
+            else
+            {
+                return "Happy";
+            }
+
+        }
+        
+
+    }
+}
--- MoodAnalyzer/MoodAnalyzer/Program.cs
+++ MoodAnalyzer/MoodAnalyzer/Program.cs
@@ -5,3 +5,11 @@ Console.WriteLine("Welcome to Mood Analyzer Problem!");
 MoodAnalyser analyse = new();
 string result = analyse.analyseMood("I am in Happy mood");
 Console.WriteLine(result);
+
+MoodAnalyzerusingConstructor analyse1 = new();
+string result1 = analyse1.analyseMood1();
+Console.WriteLine("Mood is (Using Default Constructor) : " + result1);
+
+MoodAnalyzerusingConstructor analyse2 = new();
+string result2 = analyse2.analyseMood1();
+Console.WriteLine("Mood is (Using Parameterized Constructor) : " + result2);
