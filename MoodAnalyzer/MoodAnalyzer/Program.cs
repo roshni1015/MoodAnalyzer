3dba5c306e501f8f2d9faee44234d01aca7c00f1
roshni1015 <roshniadatrao1110@gmail.com>
2022-05-20 21:44:54 +0530

    [Roshni]-Ability Analyse Happy Or Sad Mood

--- MoodAnalyzer/MoodAnalyzer/Program.cs
+++ MoodAnalyzer/MoodAnalyzer/Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using MoodAnalyzer;
+
 Console.WriteLine("Welcome to Mood Analyzer Problem!");
+MoodAnalyser analyse = new();
+string result = analyse.analyseMood("I am in Happy mood");
+Console.WriteLine(result);
